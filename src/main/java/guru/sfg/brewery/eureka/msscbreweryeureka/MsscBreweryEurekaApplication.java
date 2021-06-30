abc95850ac3ff8e984843c56efd7e6d8b1924433
pcullinan <pcullinan.ext@gema.de>
2021-06-30 15:20:31 +0200

    Notes for Service Registration with Eureka

--- src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+// add this annotation, that's all the configuration
+@EnableEurekaServer 
 @SpringBootApplication
 public class MsscBreweryEurekaApplication {
 
